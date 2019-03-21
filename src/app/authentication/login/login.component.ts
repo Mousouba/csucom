f086890e0add92eded79b6026f5b865a3a3e9377
Mousouba <mousouba@nan.ci>
2019-03-21 16:24:39 +0000

    un test du serveur

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html'
 })
-export class LoginComponent {
-  constructor() {}
+export class LoginComponent implements OnInit {
+    
+  constructor( private http: HttpClient ) {}
 
   loginform = true;
   recoverform = false;
+  
+  private const apiUrl = "http://localhost:2037/api";
+
+  data = [any];
 
   showRecoverForm() {
   	this.loginform = !this.loginform;
@@ -18,6 +26,12 @@ export class LoginComponent {
   showRecoverForm1() {
   	this.loginform = !this.loginform;
   	this.recoverform = false;
+  }
+  
+  ngOnInit(){
+    this.data = ()  => {return this.http.get(this.apiUrl)}
+    
+    console.log(this.data);
   }
 
 }
