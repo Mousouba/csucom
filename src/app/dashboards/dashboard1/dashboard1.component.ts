adaac8d0c4a581ccf6588af8ef6f20ae9e671561
Mousouba <mousouba@nan.ci>
2019-03-22 18:59:17 +0000

    receive auth

--- src/app/dashboards/dashboard1/dashboard1.component.ts
+++ src/app/dashboards/dashboard1/dashboard1.component.ts
@@ -1,11 +1,32 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Injectable, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './dashboard1.component.html',
   styleUrls: ['./dashboard1.component.css']
 })
 export class Dashboard1Component implements AfterViewInit {
-  constructor() {}
 
+  constructor(private http:HttpClient, private route:Router) {
+    this.http.get<any>("http://localhost:2037/api")
+        .subscribe( data => {
+            if(data['success'] == true){
+                route.navigate(['/authentication/login']);
+                console.log("verifié !");
+            }
+        }
+    
+    this.http.get<any>("http://localhost:2037/api")
+        .subscribe( All_data => {
+            console.log(All_data);
+        }
+        
+  }
+
+  ngOnInit() {
+    
+  }
+  
   ngAfterViewInit() {}
 }
